da2ecad26df7993ac1f5988bba697aef69c82241
bingohuang <770581715@qq.com>
2024-01-04 14:52:19 +0800

    insert record

--- 02_create_table/create_person.py
+++ 02_create_table/create_person.py
@@ -13,4 +13,25 @@ person = sqlalchemy.Table(
     sqlalchemy.Column("birthday", sqlalchemy.Date, nullable=False),
 )
 
+## create table
 meta_data.create_all(engine)
+
+## insert a record
+# person_insert = person.insert()
+# print(person_insert)
+# insert_bingo = person_insert.values(name="Bingo", birthday="1989-01-01")
+#
+# with engine.connect() as conn:
+#     result = conn.execute(insert_bingo)
+#     print(result.inserted_primary_key_rows)
+#     conn.commit()
+
+## insert multiple records
+person_insert = person.insert()
+with engine.connect() as conn:
+    conn.execute(person_insert, [
+        {"name": "bingo1", "birthday": "2024-01-01"},
+        {"name": "bingo2", "birthday": "2025-01-01"},
+        {"name": "bingo3", "birthday": "2026-01-01"},
+    ])
+    conn.commit()
